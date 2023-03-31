c098914d574069068c792d378f44350d434d7bca
KseID <I.Ksendzovskij@bssys.com>
2023-03-31 15:54:43 +0700

    Init jmix project

---
+++ src/main/kotlin/com/company/projectsandworkers/screen/user/UserBrowse.kt
@@ -0,0 +1,11 @@
+package com.company.projectsandworkers.screen.user
+
+import com.company.projectsandworkers.entity.User
+import io.jmix.ui.navigation.Route
+import io.jmix.ui.screen.*
+
+@UiController("User.browse")
+@UiDescriptor("user-browse.xml")
+@LookupComponent("usersTable")
+@Route("users")
+open class UserBrowse : StandardLookup<User>()
---
+++ src/test/kotlin/com/company/projectsandworkers/ProjectsandworkersApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.company.projectsandworkers
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+internal class ProjectsandworkersApplicationTests {
+
+    @Test
+    fun contextLoads() {
+
+    }
+}
